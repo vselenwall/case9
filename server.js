18ee65f5ed0454cbf1253b59e2569e744024a904
vselenwall <veronica@selenwall.se>
2022-11-23 21:14:19 +0100

    Initial commit
    Set up environment: routes, ejs pages, server

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
+// dependencies
 import express from 'express';
 
+// imported routes
+import routeWelcome from './routes/route-welcome.js'
+import routeLogin from './routes/route-login.js'
+import routeRegister from './routes/route-register.js'
+import routeIndex from './routes/route-index.js'
+
 const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("./public"));
 
 app.listen(5000, function() {
     console.log("Listening on port 5000");
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello, its me');
-});
+app.use('/', routeWelcome);
+app.use('/start', routeWelcome);
+app.use('/home', routeWelcome);
+
+app.use('/login', routeLogin);
+app.use('/register', routeRegister);
+app.use('/index', routeIndex);
+
+app.use((req, res, next) => {
+    res.status(404).send("404: We couldn't find this page.");
+    next();
+})
+
+app.use((req, res, next) => {
+    res.status(500).send("500: Server error - please return later.");
+    next();
+})
